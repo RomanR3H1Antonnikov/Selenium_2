2d3487025f3993f6cd346aac1d8107c07c814a23
Роман <roman.v.antonnikov@gmail.com>
2026-01-16 17:10:40 +0300

    Выполнено ДЗ: Создание класса и метода

--- OOP_1.py
+++ OOP_1.py
@@ -2,16 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
 
-class Test(): # общий класс, который будет содержать метод для работы в данном тесте
-    def test_select_product(self):
+class Test: # общий класс, который будет содержать метод для работы в данном тесте
+    def test_open_website(self):
         driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
         base_url = 'https://www.saucedemo.com/'
         driver.get(base_url)
         driver.set_window_size(1920, 1080)
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
 
 
 start_test = Test() # создаём экземпляр класса
-start_test.test_select_product() # вызов метода класса
+start_test.test_open_website() # вызов метода класса
