8db0a836c34f8a88383b316c34f3a66522feee08
Роман <roman.v.antonnikov@gmail.com>
2026-01-17 20:47:46 +0300

    Выполнено ДЗ: Создание модуля авторизации

--- OOP_3.py
+++ OOP_3.py
@@ -10,24 +10,48 @@ from selenium.webdriver.support import expected_conditions as EC
 from login_page import LoginPage
 
 
-class Test: # общий класс, который будет содержать метод для работы в данном тесте
-
-
-    def __init__(self):
+class Test:
+    """общий класс, который будет содержать метод для работы в данном тесте"""
+    def __init__(self, url='https://www.saucedemo.com/'):
         self.driver = None
 
-
-    def test_open_website(self):
+    def setup_options(self):
+        """настройка опций браузера"""
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
-        self.driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-        base_url = 'https://www.saucedemo.com/'
-        self.driver.get(base_url)
-        self.driver.set_window_size(1920, 1080)
+        return options
 
+    def setup_service(self):
+        """настройка сервиса драйвера"""
+        service = ChromeService(ChromeDriverManager().install())
+        return service
+
+    def create_driver(self, options, service):
+        """создание экземпляра драйвера"""
+        driver = webdriver.Chrome(options=options, service=service)
+        return driver
+
+    def navigate_to_url(self, url):
+        """переход по указанному URL"""
+        self.driver.get(url)
+
+    def set_window_size(self, width, height):
+        """установка размера окна браузера"""
+        self.driver.set_window_size(width, height)
+
+    def test_open_website(self):
+        """главный метод открытия сайта - использует другие методы"""
+        options = self.setup_options()
+        service = self.setup_service()
+        self.driver = self.create_driver(options, service)
+        base_url = 'https://www.saucedemo.com/'
+        self.navigate_to_url(base_url)
+        self.set_window_size(1920, 1080)
+        
+
+    def test_login(self):
         login = LoginPage(self.driver)
         login.authorization(login_name="standard_user", login_password="secret_sauce")
-
         time.sleep(3)
 
 
