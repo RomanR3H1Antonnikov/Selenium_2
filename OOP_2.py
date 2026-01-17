e7905e81294a465671c473dacd677a324ca7c5bd
Роман <roman.v.antonnikov@gmail.com>
2026-01-17 20:43:37 +0300

    Выполнено ДЗ: Smoke testing + ООП

--- OOP_2.py
+++ OOP_2.py
@@ -8,20 +8,43 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class Test: # общий класс, который будет содержать метод для работы в данном тесте
-
-
-    def __init__(self):
+class Test:
+    """общий класс, который будет содержать метод для работы в данном тесте"""
+    def __init__(self, url='https://www.saucedemo.com/'):
         self.driver = None
 
-
-    def test_open_website(self):
+    def setup_options(self):
+        """настройка опций браузера"""
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
-        self.driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+        return options
+
+    def setup_service(self):
+        """настройка сервиса драйвера"""
+        service = ChromeService(ChromeDriverManager().install())
+        return service
+
+    def create_driver(self, options, service):
+        """создание экземпляра драйвера"""
+        driver = webdriver.Chrome(options=options, service=service)
+        return driver
+
+    def navigate_to_url(self, url):
+        """переход по указанному URL"""
+        self.driver.get(url)
+
+    def set_window_size(self, width, height):
+        """установка размера окна браузера"""
+        self.driver.set_window_size(width, height)
+
+    def test_open_website(self):
+        """главный метод открытия сайта - использует другие методы"""
+        options = self.setup_options()
+        service = self.setup_service()
+        self.driver = self.create_driver(options, service)
         base_url = 'https://www.saucedemo.com/'
-        self.driver.get(base_url)
-        self.driver.set_window_size(1920, 1080)
+        self.navigate_to_url(base_url)
+        self.set_window_size(1920, 1080)
 
 
     def test_website_authorization(self):
